e91c135f5a8a990df2413ffcc3d042c30aed308d
d4t06 <huudat01234560@gamail.com>
2024-06-25 00:34:51 +0700

    update for deploy 1

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +8,6 @@ async function bootstrap() {
     origin: ['http://localhost:3000', 'https://d4t06.github.io'],
   });
 
-  app.use(cookieParser());
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT || 4000, () => {
     console.log(`App running on port:${process.env.PORT}`);
