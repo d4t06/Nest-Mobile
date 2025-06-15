9fc62776470b6384a62604c010e075674567f188
d4t06 <huudat01234560@gmail.com>
2025-06-15 20:34:45 +0700

    Updates

--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,21 +29,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const dotenv = __importStar(require("dotenv"));
+dotenv.config({
+    path: 'env.local',
+});
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const whiteList = process.env.WHITE_LIST.split(', ') || [];
     app.enableCors({
         credentials: true,
-        origin: [
-            'http://localhost:3000',
-            'https://next-mobile-ebon.vercel.app',
-            'https://nuxt-mobile.netlify.app',
-        ],
+        origin: [...whiteList],
     });
     app.use((0, cookie_parser_1.default)());
     app.setGlobalPrefix('api');
     const PORT = process.env.PORT || 4000;
     await app.listen(PORT, () => {
-        console.log(`App running on port:${PORT}`);
+        console.log(`App running on port: ${PORT}, whiteList: ${whiteList}`);
     });
 }
 bootstrap();
